27ee629ef382913240ab5d018e04855b3c9966aa
jade-r-k <jade.r.s.kelly@gmail.com>
2023-02-28 15:01:00 +0000

    Importing pages and setting up routes

--- src/App.js
+++ src/App.js
@@ -3,12 +3,29 @@ import { useState, useEffect } from 'react';
 
 //pages
 import Home from './pages/Home';
+//static pages
+import StaticHome from './pages/static/Home';
+import StaticForm from './pages/static/Form';
+import StaticGallery from './pages/static/Gallery';
+//interactive pages
+import InteractiveHome from './pages/interactive/Home'
+import InteractiveForm from './pages/interactive/Form'
+import InteractiveGallery from './pages/interactive/Gallery'
+
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path='/' element={<Home />} />
+
+        <Route path='/static/' element={<StaticHome />} />
+        <Route path='/static/form' element={<StaticForm />} />
+        <Route path='/static/gallery' element={<StaticGallery />} />
+
+        <Route path='/interactive/' element={<InteractiveHome />} />
+        <Route path='/interactive/form' element={<InteractiveForm />} />
+        <Route path='/interactive/gallery' element={<InteractiveGallery />} />
       </Routes>
     </Router>
   );
