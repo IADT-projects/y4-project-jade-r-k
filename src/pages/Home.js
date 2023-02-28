8aeaa6ecd5ffa455df966eb2637ccdc141988c07
jade-r-k <jade.r.s.kelly@gmail.com>
2023-02-28 18:52:17 +0000

    button links

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import axios from '../config';
 import { useState, useEffect } from 'react';
 import { Button, Stack } from '@mui/material';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
     const [artworks, setArtworks ] = useState(null);
@@ -20,9 +21,10 @@ const Home = () => {
         <h1 className="f1 center-text">Welcome to the comparative analysis of static and interactive design study</h1>
         <h2 className="f2 center-text">Thank you for taking part. You will be told which button to click below in order to begin the study.</h2>
         <br></br>
+        <br></br>
         <Stack spacing={2} justifyContent="center" direction="row">
-        <Button variant="contained">Static</Button>
-        <Button variant="contained">Interactive</Button>
+        <Button component={Link} to='/static/' variant="contained">Static</Button>
+        <Button component={Link} to='/interactive/' variant="contained">Interactive</Button>
         </Stack>
         </>
     );
