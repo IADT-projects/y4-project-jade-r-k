5f03e7d9659b1e49bd358ba737ccf570bd7f7a25
jade-r-k <jade.r.s.kelly@gmail.com>
2023-02-28 14:55:34 +0000

    index.js setup

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { CssBaseline } from '@mui/material';
+
+import '@fontsource/playfair-display/300.css';
+import '@fontsource/playfair-display/400.css';
+import '@fontsource/playfair-display/500.css';
+import '@fontsource/playfair-display/700.css';
+
+import '@fontsource/source-sans-pro/300.css';
+import '@fontsource/source-sans-pro/400.css';
+import '@fontsource/source-sans-pro/500.css';
+import '@fontsource/source-sans-pro/700.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <CssBaseline />
     <App />
   </React.StrictMode>
 );
