f22b1ed3a9b4487ac4b19105edc9ee48b0249d6d
jade-r-k <jade.r.s.kelly@gmail.com>
2023-03-07 22:03:52 +0000

    Styling

--- src/components/StaticCards.js
+++ src/components/StaticCards.js
@@ -7,27 +7,39 @@ const StaticCards = () => {
     return (
         <>
         <CardGroup>
-          <Card className="center text-center">
-            <Card.Img src="https://images.unsplash.com/photo-1605721911519-3dfeb3be25e7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8YXJ0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=900&q=60"/>
+          <Card className="center text-center" style={{height: '300px', margin: '10px'}}>
+            <Card.Img style={{height: '300px'}} src="https://images.unsplash.com/photo-1605721911519-3dfeb3be25e7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8YXJ0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=900&q=60"/>
             <Card.ImgOverlay>
               <Card.Body>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
               <Button className="f2" component={Link} to='/static/gallery' variant="contained">Gallery</Button>
               </Card.Body>
             </Card.ImgOverlay>
           </Card>
-          <Card className="center text-center">
-            <Card.Img src="https://images.unsplash.com/photo-1605721911519-3dfeb3be25e7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8YXJ0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=900&q=60"/>
+          <Card className="center text-center" style={{height: '300px', margin: '10px'}}>
+            <Card.Img style={{height: '300px'}} src="https://images.unsplash.com/photo-1605721911519-3dfeb3be25e7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8YXJ0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=900&q=60"/>
             <Card.ImgOverlay>
               <Card.Body>
-              <Button className="f2" component={Link} to='/static/gallery' variant="contained">Gallery</Button>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+              <Button className="f2" component={Link} to='/static/gallery' variant="contained">Contact Form</Button>
               </Card.Body>
             </Card.ImgOverlay>
           </Card>
-          <Card className="center text-center">
-            <Card.Img src="https://images.unsplash.com/photo-1605721911519-3dfeb3be25e7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8YXJ0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=900&q=60"/>
+          <Card className="center text-center" style={{height: '300px', margin: '10px'}}>
+            <Card.Img style={{height: '300px'}} src="https://images.unsplash.com/photo-1605721911519-3dfeb3be25e7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8YXJ0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=900&q=60"/>
             <Card.ImgOverlay>
               <Card.Body>
-              <Button className="f2" component={Link} to='/static/gallery' variant="contained">Gallery</Button>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+              <Button className="f2" component={Link} to='/interactive/' variant="contained">Interactive</Button>
               </Card.Body>
             </Card.ImgOverlay>
           </Card>
