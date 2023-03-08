31c3e1e48367f7c127a8e30a0e97dc798c07079b
jade-r-k <jade.r.s.kelly@gmail.com>
2023-03-08 17:48:27 +0000

    restarting project

--- src/App.js
+++ src/App.js
@@ -2,14 +2,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 //pages
 import Home from './pages/Home';
-//static pages
-import StaticHome from './pages/static/Home';
-import StaticForm from './pages/static/Form';
-import StaticGallery from './pages/static/Gallery';
-//interactive pages
-import InteractiveHome from './pages/interactive/Home'
-import InteractiveForm from './pages/interactive/Form'
-import InteractiveGallery from './pages/interactive/Gallery'
 
 
 function App() {
@@ -18,14 +10,6 @@ function App() {
       <Routes>
         {/* Routes to pages */}
         <Route path='/' element={<Home />} />
-
-        <Route path='/static/' element={<StaticHome />} />
-        <Route path='/static/form' element={<StaticForm />} />
-        <Route path='/static/gallery' element={<StaticGallery />} />
-
-        <Route path='/interactive/' element={<InteractiveHome />} />
-        <Route path='/interactive/form' element={<InteractiveForm />} />
-        <Route path='/interactive/gallery' element={<InteractiveGallery />} />
       </Routes>
     </Router>
   );
