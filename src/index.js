96efc2d3e42874be18495f6f618f6f20f9376d54
jade-r-k <jade.r.s.kelly@gmail.com>
2023-03-08 17:48:59 +0000

    restarting project

--- src/index.js
+++ src/index.js
@@ -1,18 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { CssBaseline } from '@mui/material';
 
 import './css/App.css';
-
-//fonts
-import '@fontsource/playfair-display';
-import '@fontsource/source-sans-pro/400.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <CssBaseline />
     <App />
   </React.StrictMode>
 );
